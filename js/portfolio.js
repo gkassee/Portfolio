e213ec2cd1468e252e03103f61b391ff4145b2ea
Greg Kassee <gkassee@gmail.com>
2016-07-03 14:24:23 -0400

    Adding smoother jQuery animations, updated styling, and links to project sites

--- js/portfolio.js
+++ js/portfolio.js
@@ -19,10 +19,12 @@ $(document).ready(function() {
 
 	$('#contact-button').click(function() {
 		$('#contact-wrapper').animate({'background-color': '#0DC478'}, 1000);
+		$('#my_name').animate({'text-shadow': '0px -1px #0DC478, 0px -2px #0DC478, 0px -3px #0DC478, 0px -4px #0DC478, 0px -5px #0DC478'}, 500);
+		$('#my_title').css({'color':'#0DC478'}, 500);
 		$('#contact-button').animate({'color': '#0DC478'}, 500);
 
 
-		$('#projects-wrapper').animate({'height': '10%', 'background-color': 'white'}, 700);
+		$('#projects-wrapper').delay(200).animate({'height': '10%', 'background-color': 'white'}, 600);
 		$('#projects-button').animate({'color': 'white'}, 500);
 
 
@@ -33,7 +35,9 @@ $(document).ready(function() {
 
 
 	$('#projects-button').click(function() {
-		$('#projects-wrapper').animate({'height': '78%', 'background-color': '#0095F9', 'color': 'white'}, 560);
+		$('#projects-wrapper').animate({'height': '77%', 'background-color': '#0095F9'}, 560);
+		$('#my_name').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9, 0px -5px #0095F9'}, 500);
+		$('#my_title').css({'color':'#0095F9'}, 500);
 		$('#projects-button').animate({'color': '#0095F9'}, 500);
 
 
@@ -49,7 +53,9 @@ $(document).ready(function() {
 
 
 	$('#bio-button').click(function() {
-		$('#bio-wrapper').animate({'height': '75%', 'background-color': '#FF4956'}, 680);
+		$('#bio-wrapper').delay(200).animate({'height': '74%', 'background-color': '#FF4956'}, 600);
+		$('#my_name').animate({'text-shadow': '0px -1px #FF4956, 0px -2px #FF4956, 0px -3px #FF4956, 0px -4px #FF4956, 0px -5px #FF4956'}, 500);
+		$('#my_title').css({'color':'#FF4956'}, 500);
 		$('#bio-button').animate({'color': '#FF4956'}, 700);
 
 
@@ -57,7 +63,7 @@ $(document).ready(function() {
 		$('#contact-button').animate({'color': 'white'}, 500);
 
 
-		$('#projects-wrapper').animate({'height': '78%', 'background-color': 'white'}, 600);
+		$('#projects-wrapper').animate({'height': '77%', 'background-color': 'white'}, 600);
 		$('#projects-button').animate({'color': 'white'}, 500);
 
 	}) 
