e8738f6c4d73cf8f77c0e6b38efebd79394a0b01
Greg Kassee <gkassee@gmail.com>
2016-07-08 03:08:12 -0400

    Adding adding jquery animation fix for responsive styles

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,103 +15,15 @@
 // })
 
 $(document).ready(function() {
+  // run screen size test on initial page load
+  checkSize();
+  checkSize2();
 
-	$(document).ready(function() {
-    // run screen size test on initial page load
-    checkSize();
+  // run test on resize of the window
+  $(window).resize(checkSize);
+  $(window).resize(checkSize2);
 
-    // run test on resize of the window
-    $(window).resize(checkSize);
-	});
-
-
-	$('#contact-button').click(function() {
-		$('#contact-wrapper').animate({'background-color': '#0DC478'}, 500);
-
-		$('body').css('background', 'url(https://s31.postimg.org/ns9l9namz/contact_pattern.png)');
-
-		$('#modal').css('opacity', '0.92');
-
-		$('.my_name').removeClass("my_name_red");
-		$('.my_name').removeClass("my_name_blue");
-		$('.my_name').addClass("my_name_green");
-		$('#my_title').css({'color':'#0DC478'}, 500);
-		$('#contact-button').animate({'color': '#0DC478'}, 500);
-
-
-		$('#projects-wrapper').delay(170).animate({'height': '6%', 'background-color': 'white'}, 400);
-		$('.tool').animate({'background-color': 'white'}, 600);
-		$('.screens').animate({'opacity': '0'}, 500);
-		$('#projects-button').animate({'color': 'white'}, 500);
-
-
-		$('#bio-wrapper').animate({'height': '3%', 'background-color': 'white'}, 400);
-		$('#bio-button').animate({'color': 'white'}, 500);
-	})
-
-
-
-	$('#projects-button').click(function() {
-		$('#projects-wrapper').animate({'height': '77%', 'background-color': '#0095F9'}, 500);
-		$('.tool').animate({'background-color': '#FF4956'}, 800);
-		$('.screens').animate({'opacity': '1'}, 800);
-
-		$('body').css('background', 'url(https://s32.postimg.org/6ds3m8pxh/projects_pattern.png)');
-
-		$('#modal').css('opacity', '0.92');
-
-		// $('#bandly').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
-
-		// $('#neighborino').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
-
-		// $('#jukebox').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
-
-		$('.my_name').removeClass("my_name_green");
-		$('.my_name').removeClass("my_name_red");
-		$('.my_name').addClass("my_name_blue");
-		$('#my_title').css({'color':'#0095F9'}, 500);
-		$('#projects-button').animate({'color': '#0095F9'}, 500);
-
-
-		$('#contact-wrapper').animate({'background-color': 'white'}, 500);
-		$('#contact-button').animate({'color': 'white'}, 500);
-
-
-		$('#bio-wrapper').animate({'height': '3%', 'background-color': 'white'}, 400);
-		$('#bio-button').animate({'color': 'white'}, 500);
-	}) 
-
-
-
-	$('#bio-button').click(function() {
-		$('#bio-wrapper').delay(170).animate({'height': '74%', 'background-color': '#FF4956'}, 350);
-
-		$('body').css('background', 'url(https://s-media-cache-ak0.pinimg.com/236x/c5/31/0c/c5310cd8e766595915405683d7bd3cb1.jpg)');
-
-		$('#modal').css('opacity', '0.97');
-
-		$('.my_name').removeClass("my_name_green");
-		$('.my_name').removeClass("my_name_blue");
-		$('.my_name').addClass("my_name_red");
-		$('#my_title').css({'color':'#FF4956'}, 500);
-		$('#bio-button').animate({'color': '#FF4956'}, 500);
-
-
-		$('#contact-wrapper').animate({'background-color': 'white'}, 500);
-		$('#contact-button').animate({'color': 'white'}, 500);
-
-
-		$('#projects-wrapper').animate({'height': '77%', 'background-color': 'white'}, 350);
-		$('.tool').animate({'background-color': 'white'}, 600);
-		$('.screens').animate({'opacity': '0'}, 500);
-		$('#projects-button').animate({'color': 'white'}, 500);
-	}) 
-
-
-
-
-
-	// Project Descript Switches
+  // Project Descript Switches
 
 	$('#bandly').click(function() {
 		$('#neighborino-content-wrapper').hide();
@@ -139,22 +51,116 @@ $(document).ready(function() {
 		$('#neighborino').css('border-bottom', 'none');
 		$('#bandly').css('border-bottom', 'none');
 	})
-		
-	
-})
+
+});
+
+
+function checkSize(){
+	if ($("#contacters-1").css("left") == 'none' ){
+
+		$('#contact-button').click(function() {
+			$('#contact-wrapper').animate({'background-color': '#0DC478'}, 500);
+
+			$('body').css('background', 'url(https://s31.postimg.org/ns9l9namz/contact_pattern.png)');
+
+			$('#modal').css('opacity', '0.92');
+
+			$('.my_name').removeClass("my_name_red");
+			$('.my_name').removeClass("my_name_blue");
+			$('.my_name').addClass("my_name_green");
+			$('#my_title').css({'color':'#0DC478'}, 500);
+			$('#contact-button').animate({'color': '#0DC478'}, 500);
+
+
+			$('#projects-wrapper').delay(170).animate({'height': '6%', 'background-color': 'white'}, 400);
+			$('.tool').animate({'background-color': 'white'}, 600);
+			$('.screens').animate({'opacity': '0'}, 500);
+			$('#projects-button').animate({'color': 'white'}, 500);
+
+
+			$('#bio-wrapper').animate({'height': '3%', 'background-color': 'white'}, 400);
+			$('#bio-button').animate({'color': 'white'}, 500);
+		})
+
+
+
+		$('#projects-button').click(function() {
+			$('#projects-wrapper').animate({'height': '77%', 'background-color': '#0095F9'}, 500);
+			$('.tool').animate({'background-color': '#FF4956'}, 800);
+			$('.screens').animate({'opacity': '1'}, 800);
+
+			$('body').css('background', 'url(https://s32.postimg.org/6ds3m8pxh/projects_pattern.png)');
+
+			$('#modal').css('opacity', '0.92');
+
+			// $('#bandly').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
+
+			// $('#neighborino').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
+
+			// $('#jukebox').animate({'text-shadow': '0px -1px #0095F9, 0px -2px #0095F9, 0px -3px #0095F9, 0px -4px #0095F9'}, 560)
+
+			$('.my_name').removeClass("my_name_green");
+			$('.my_name').removeClass("my_name_red");
+			$('.my_name').addClass("my_name_blue");
+			$('#my_title').css({'color':'#0095F9'}, 500);
+			$('#projects-button').animate({'color': '#0095F9'}, 500);
+
+
+			$('#contact-wrapper').animate({'background-color': 'white'}, 500);
+			$('#contact-button').animate({'color': 'white'}, 500);
+
+
+			$('#bio-wrapper').animate({'height': '3%', 'background-color': 'white'}, 400);
+			$('#bio-button').animate({'color': 'white'}, 500);
+		}) 
+
+
+
+		$('#bio-button').click(function() {
+			$('#bio-wrapper').delay(170).animate({'height': '74%', 'background-color': '#FF4956'}, 350);
+
+			$('body').css('background', 'url(https://s-media-cache-ak0.pinimg.com/236x/c5/31/0c/c5310cd8e766595915405683d7bd3cb1.jpg)');
+
+			$('#modal').css('opacity', '0.97');
+
+			$('.my_name').removeClass("my_name_green");
+			$('.my_name').removeClass("my_name_blue");
+			$('.my_name').addClass("my_name_red");
+			$('#my_title').css({'color':'#FF4956'}, 500);
+			$('#bio-button').animate({'color': '#FF4956'}, 500);
+
+
+			$('#contact-wrapper').animate({'background-color': 'white'}, 500);
+			$('#contact-button').animate({'color': 'white'}, 500);
+
+
+			$('#projects-wrapper').animate({'height': '77%', 'background-color': 'white'}, 350);
+			$('.tool').animate({'background-color': 'white'}, 600);
+			$('.screens').animate({'opacity': '0'}, 500);
+			$('#projects-button').animate({'color': 'white'}, 500);
+		}) 
+	}
+}
+
+
+
 
 
 
 
 
 //Function to the css rule
-function checkSize(){
-    if ($("#links").css("width") == '100%' ){
+function checkSize2(){
+    if ($("#contacters-1").css("float") == 'none' ){
         
 			  $('#contact-button').click(function() {
-			  	$('#projects-wrapper').hide();
-			  	$('#bio-wrapper').hide();
-					$('#contact-wrapper').show();
+			  // 	$('#projects-wrapper').hide();
+			  // 	$('#bio-wrapper').hide();
+					// $('#contact-wrapper').show();
+
+					$('#bio-wrapper').css('visibility', 'hidden');
+					$('#projects-wrapper').css('visibility', 'hidden');
+					$('#contact-wrapper').css('visibility', 'visible');
 
 					$('body').css('background', 'url(https://s31.postimg.org/ns9l9namz/contact_pattern.png)');
 
@@ -175,9 +181,13 @@ function checkSize(){
 
 
 				$('#projects-button').click(function() {
-					$('#contact-wrapper').hide();
-			  	$('#bio-wrapper').hide();
-					$('#projects-wrapper').show();
+					// $('#contact-wrapper').hide();
+			  // 	$('#bio-wrapper').hide();
+					// $('#projects-wrapper').show();
+
+					$('#contact-wrapper').css('visibility', 'hidden');
+					$('#bio-wrapper').css('visibility', 'hidden');
+					$('#projects-wrapper').css('visibility', 'visible');
 
 					$('body').css('background', 'url(https://s32.postimg.org/6ds3m8pxh/projects_pattern.png)');
 
@@ -198,9 +208,13 @@ function checkSize(){
 
 
 				$('#bio-button').click(function() {
-					$('#contact-wrapper').hide();
-					$('#projects-wrapper').hide();
-					$('#bio-wrapper').show();
+					// $('#contact-wrapper').hide();
+					// $('#projects-wrapper').hide();
+					// $('#bio-wrapper').show();
+
+					$('#contact-wrapper').css('visibility', 'hidden');
+					$('#projects-wrapper').css('visibility', 'hidden');
+					$('#bio-wrapper').css('visibility', 'visible');
 
 					$('body').css('background', 'url(https://s-media-cache-ak0.pinimg.com/236x/c5/31/0c/c5310cd8e766595915405683d7bd3cb1.jpg)');
 
